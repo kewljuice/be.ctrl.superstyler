fea25c99e63acc94c10d74647ed697cfd46fdae6
kewljuice <kewljuice@hotmail.com>
2016-01-29 16:09:07 +0100

    Created callback page for file upload

---
+++ CRM/superstyler/Page/superstyler/upload.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'CRM/Core/Page.php';
+
+class CRM_superstyler_Page_superstyler_upload extends CRM_Core_Page {
+  function run() {
+
+    // Declare JSON object.
+    $json = array('is_error' => 1);
+
+    // Handle file upload.
+
+
+    // Dump JSON.
+    print json_encode($json, JSON_PRETTY_PRINT);
+
+    // Exit CRM_Core_Page flow.
+    CRM_Utils_System::civiExit();
+  }
+}
