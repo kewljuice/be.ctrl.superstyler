ae3e3dc54ae6a099329c0659b276c383abd1f2b1
kewljuice <no@ctrl.be>
2016-02-10 17:12:52 +0100

    Fixed Issue with unique child keys in ctrl menu

--- php/hooks.php
+++ php/hooks.php
@@ -2,57 +2,70 @@
 /**
  * CiviCRM hooks
  */
-function superstyler_civicrm_navigationMenu(&$params)
-{
-    //  Get the maximum key of $params.
-    $nextKey = (max(array_keys($params)));
-    // Check for Administer navID.
-    $AdministerKey = '';
-    foreach ($params as $k => $v) {
-        if ($v['attributes']['name'] == 'Administer') {
-            $AdministerKey = $k;
-        }
+function superstyler_civicrm_navigationMenu(&$params) {
+
+  // Get the maximum key of $params.
+  $NextKey = (max(array_keys($params)));
+  // Get Next available item.
+  $NextKey++;
+
+  // Check for Administer navID.
+  $AdministerKey = 0;
+  foreach ($params as $k => $v) {
+    if ($v['attributes']['name'] == 'Administer') {
+      $AdministerKey = $k;
     }
-    // Check for Parent navID.
-    foreach ($params[$AdministerKey]['child'] as $k => $v) {
-        if ($v['attributes']['name'] == 'CTRL') {
-            $parentKey = $v['attributes']['navID'];
-        }
+  }
+
+  // Check for Parent navID.
+  $ParentKey = 0;
+  foreach ($params[$AdministerKey]['child'] as $k => $v) {
+    if ($v['attributes']['name'] == 'CTRL') {
+      $ParentKey = $v['attributes']['navID'];
     }
-    // If Parent navID doesn't exist create.
-    if (!isset($parentKey)) {
-        // Create parent array
-        $parent = array(
-            'attributes' => array(
-                'label' => 'CTRL',
-                'name' => 'CTRL',
-                'url' => null,
-                'permission' => 'access CiviCRM',
-                'operator' => null,
-                'separator' => 0,
-                'parentID' => $AdministerKey,
-                'navID' => $nextKey,
-                'active' => 1),
-            'child' => null);
-        // Add parent to Administer
-        $params[$AdministerKey]['child'][$nextKey] = $parent;
-        $parentKey = $nextKey;
-        $nextKey++;
-    }
-    // Create child(s) array
-    $child = array(
-        'attributes' => array(
-            'label' => 'SuperStyler',
-            'name' => 'ctrl_superstyler',
-            'url' => 'civicrm/ctrl/superstyler',
-            'permission' => 'access CiviCRM',
-            'operator' => null,
-            'separator' => 0,
-            'parentID' => $parentKey,
-            'navID' => $nextKey,
-            'active' => 1),
-        'child' => null);
-    // Add child(s) for this extension
-    $params[$AdministerKey]['child'][$parentKey]['child'][$nextKey] = $child;
+  }
+
+  // If Parent navID doesn't exist create.
+  if ($ParentKey == 0) {
+    // Create parent array.
+    $parent = array(
+      'attributes' => array(
+        'label' => 'CTRL',
+        'name' => 'CTRL',
+        'url' => NULL,
+        'permission' => 'access CiviCRM',
+        'operator' => NULL,
+        'separator' => 0,
+        'parentID' => $AdministerKey,
+        'navID' => $NextKey,
+        'active' => 1
+      ),
+      'child' => NULL
+    );
+    // Add parent to Administer.
+    $params[$AdministerKey]['child'][$NextKey] = $parent;
+    // Define parentKey & nextKey.
+    $ParentKey = $NextKey;
+  }
+
+  // Create child array.
+  $child = array(
+    'attributes' => array(
+      'label' => 'SuperStyler',
+      'name' => 'ctrl_superstyler',
+      'url' => 'civicrm/ctrl/superstyler',
+      'permission' => 'access CiviCRM',
+      'operator' => NULL,
+      'separator' => 0,
+      'parentID' => $parentKey,
+      'navID' => $nextKey,
+      'active' => 1
+    ),
+    'child' => NULL
+  );
+
+  // Add child(s) for this extension.
+  $params[$AdministerKey]['child'][$ParentKey]['child']['ctrl_superstyler'] = $child;
 }
+
 ?>
